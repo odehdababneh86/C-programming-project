a9a1d7858400604d9116d333fa2a5d86b61c88eb
odehdababneh86 <odehdababneh86@gmail.com>
2024-12-16 09:50:57 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void make_upper(char *s);
+
+int main()
+{
+
+  char s[] = "Some string with many lowercase LETTERS";
+  make_upper(s);
+  printf("%s\n", s);
+
+  return 0;
+}
+
+void make_upper(char *s)
+{
+  int length = strlen(s);
+
+  for (int i = 0; i < length; i++)
+    s[i] = toupper(s[i]);
+}
